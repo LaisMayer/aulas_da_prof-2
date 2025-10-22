4e9873ad6dc4773acf046d7541660d73bdfaa47e
EliasCardoso-2ano <elias.cardoso.da.silva.junior17@gmail.com>
2025-10-22 09:01:31 -0300

    .

---
+++ exercicio_1.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class exercicio_1 {
+final static Scanner LER = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        for (int mensagem = 0; mensagem <= 20; mensagem++) {
+            System.out.println("“Esta é a mensagem " + mensagem);
+        }
+    }
+}
